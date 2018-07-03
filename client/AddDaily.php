d17972db31e470bc947b70ba0d16bbc6af223216
da2022 <40353545+da2022@users.noreply.github.com>
2018-07-03 14:53:33 +0100

    Add files via upload

--- client/AddDaily.php
+++ client/AddDaily.php
@@ -16,9 +16,38 @@ echo $_SESSION['username'];
 
 
 <form action="AddDaily.php" method = "post" enctype = "multipart/form-data">
-<input type = "file" name = "FileUpload" id = "FileUpload">
-<input type = "submit" value = "Upload file" name = "submit">
+<input type = "file" name = "file" id = "FileUpload">
+<input type = "submit" value = "Uploadfile" name = "submit">
 
+
+<?php
+#if(isset($_POST['submit']))
+#{
+	##############This doesnt work yet and idk why but ill figure it out
+	#$file = $_FILES['file']['name'];
+	#$file_loc = $_FILES['file']['tmp_name'];
+	#$file_size = $_FILES['file']['size'];
+	#$file_type = $_FILES['file']['type'];
+	#$folder = "uploads/";
+	
+	
+	#move_uploaded_file($file_loc,$folder.$file);
+	#$sql="INSERT INTO ----------- VALUES('$file','$file_type','$file_size')";
+	######################
+	
+	#$servername = '-------';
+	#$username = '----';
+	#$password = '--------';
+	#$db_name = '------';
+
+	#$con = mysqli_connect($servername, $username, $password, $db_name);
+
+
+
+	#mysql_query($sql);
+#}
+
+?>
 
 </div>
 
--- client/AddNews.php
+++ client/AddNews.php
@@ -20,6 +20,36 @@ echo $_SESSION['username'];
 <input type = "submit" value = "Upload file" name = "submit">
 
 
+<?php
+#if(isset($_POST['submit']))
+#{
+	##############This doesnt work yet and idk why but ill figure it out
+	#$file = $_FILES['file']['name'];
+	#$file_loc = $_FILES['file']['tmp_name'];
+	#$file_size = $_FILES['file']['size'];
+	#$file_type = $_FILES['file']['type'];
+	#$folder = "uploads/";
+	
+	
+	#move_uploaded_file($file_loc,$folder.$file);
+	#$sql="INSERT INTO ----------- VALUES('$file','$file_type','$file_size')";
+	######################
+	
+	#$servername = '-------';
+	#$username = '----';
+	#$password = '--------';
+	#$db_name = '------';
+
+	#$con = mysqli_connect($servername, $username, $password, $db_name);
+
+
+
+	#mysql_query($sql);
+#}
+
+?>
+
+
 </div>
 
 </body>
