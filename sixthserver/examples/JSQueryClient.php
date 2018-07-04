387d7b137da0b381fee821840ad392e97a288998
fboyle2001 <finlayboyle2001@gmail.com>
2018-07-04 19:09:38 +0100

    Javascript login example code

---
+++ sixthserver/examples/JSQueryClient.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<script src="jquery-3.2.1.min.js"></script>
+		<script src="login.js"></script>
+	</head>
+	<body>
+		<p id="last"></p>
+		<div>
+			<h3>Login Data</h3>
+			<p>Username: <span id="username"></span></p>
+			<p>Password: <span id="password"></span></p>
+			<p>Auth Key: <span id="auth"></span></p>
+		</div>
+	</body>
+</html>
