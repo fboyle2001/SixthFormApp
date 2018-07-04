020e5aaf35ef78207e3e849df4f6bfa37198a3c5
fboyle2001 <finlayboyle2001@gmail.com>
2018-07-04 14:00:46 +0100

    Admin page basics

---
+++ sixthadmin/resources/php/body.php
@@ -0,0 +1,12 @@
+<div class="navigation">
+  <nav>
+    <ul>
+      <li data-content-key="accounts">Accounts</li>
+      <li data-content-key="announcements">Announcements</li>
+			<li data-content-key="calendar">Calendar</li>
+			<li data-content-key="files">Files</li>
+			<li data-content-key="links">Links</li>
+		</ul>
+  </nav>
+</div>
+<div class="subnav" id="subnav" style="display: none;"></div>
