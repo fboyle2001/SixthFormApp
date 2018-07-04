863e9450727893269c8e85582dae3f3734593ea5
fboyle2001 <fboyle2001@users.noreply.github.com>
2018-07-04 12:03:22 +0100

    Announcement Retrieval

---
+++ sixthserver/api/fetch/announcements/list/index.php
@@ -0,0 +1,27 @@
+<?php
+  include($_SERVER["DOCUMENT_ROOT"] . "/sixthserver/api/api_util.php");
+	
+	$reply = new Reply();
+		
+  if(!validate(AccessLevel::student)) {
+    $status = ReplyStatus::withData(403, "Unauthorised access is restricted");
+    $reply->setStatus($status);
+    die($reply->toJson());
+  }
+	
+	$limit = post("limit");
+	
+	if($limit == null) {
+		$reply->setStatus(ReplyStatus::withData(400, "Must set a limit"));
+		die($reply->toJson());
+	}
+	
+	$selectLatest = "SELECT * FROM `announcements` ORDER BY `ID` DESC LIMIT $limit";
+	$selectLatest = DatabaseHandler::getInstance()->executeQuery($selectLatest);
+	
+	$reply->setStatus(ReplyStatus::withData(200, "Success"));
+	$reply->setValue("found", $selectLatest->wasDataReturned());
+	$reply->setValue("records", $selectLatest->getRecords());
+	
+	echo $reply->toJson();	
+?>
