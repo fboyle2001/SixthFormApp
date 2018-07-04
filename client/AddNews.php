6c12e13ede731d5551006ab3424b98ef311d1e02
da2022 <40353545+da2022@users.noreply.github.com>
2018-07-04 11:15:19 +0100

    Add files via upload
    just needs to be in communication with the server, all the needed variables for each are in the echo below the form

--- client/AddNews.php
+++ client/AddNews.php
@@ -1,55 +1,36 @@
-<?php
-
-session_start();
-
-if (empty($_SESSION['username'])) {
-$_SESSION['username'] = 'Guest';
-}
-
-echo $_SESSION['username'];
-
-?>
-
 <html>
+
+
+
 
 <body>
 
 
-<form action="AddNews.php" method = "post" enctype = "multipart/form-data">
-<input type = "file" name = "file" id = "FileUpload">
-<input type = "submit" value = "Uploadfile" name = "submit">
 
+
+<form method="post" action="AddNews.php" enctype="multipart/form-data">
+    <p>File :</p>
+    <input type="file" accept ="application/pdf" name="File"> 
+	<p>Expiry Date :</p>
+	<input type "date" name ="ExpDate">
+    <input TYPE="submit" name="upload" value="Submit"/>
+</form>
+
+
+ 
 
 <?php
-if(isset($_POST['submit']))
-{
-	
-	$file = $_FILES['file']['name'];
-	$file_loc = $_FILES['file']['tmp_name'];
-	$folder = "uploads/";
-	
-	
-	#move_uploaded_file($file_loc,$folder.$file);
-	#$sql="INSERT INTO ----------- VALUES('$file',)";
-	
-	
-	#$servername = '-------';
-	#$username = '----';
-	#$password = '--------';
-	#$db_name = '------';
-
-	#$con = mysqli_connect($servername, $username, $password, $db_name);
 
 
-
-	#mysql_query($sql);
-}
+	echo date("d/m/y");
+	echo $_POST['ExpDate'];
+	echo $_FILES['File']['name'];
+	
 
 ?>
 
 
-</div>
-
 </body>
+
 
 </html>
