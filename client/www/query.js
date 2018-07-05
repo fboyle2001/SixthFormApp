9ed5c5dd0222ba608b9ababf099fdae472c67a58
fboyle2001 <finlayboyle2001@gmail.com>
2018-07-05 15:06:57 +0100

    query.js added

---
+++ client/www/query.js
@@ -0,0 +1,42 @@
+function isLoggedIn() {
+  if(Cookies.get("auth") === undefined) {
+    return false;
+  }
+
+  if(Cookies.get("base") === undefined) {
+    return false;
+  }
+
+  return true;
+}
+
+function query(url, postData, callback, fatal) {
+  if(!isLoggedIn()) {
+    var response = {
+      "status": {
+        "code": 401,
+        "description": "User is not logged in"
+      },
+      "content": {}
+    };
+
+    fatal(response);
+    return;
+  }
+
+  $.ajax({
+    url: Cookies.get("base") + url,
+    type: "post",
+    dataType: "json",
+    data: postData,
+    headers: {
+      authorization: Cookies.get("auth")
+    },
+    success: function(data) {
+      callback(data);
+    },
+    error: function(data) {
+      fatal(data);
+    }
+  });
+}
