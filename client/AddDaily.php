cf212a78085dad59384cc8f957b1ff2fc8b7b8c5
da2022 <40353545+da2022@users.noreply.github.com>
2018-07-05 09:10:06 +0100

    Add files via upload
    this will take a pdf document and get the information about it and put that in to the database, also hashed below is the code to display this pdf document

--- client/AddDaily.php
+++ client/AddDaily.php
@@ -1,51 +1,85 @@
-<html>
-
-
-
-
-<body>
-
-
-
-
-<form method="post" action="AddDaily.php" enctype="multipart/form-data">
-    <p>File :</p>
-    <input type="file" accept ="application/pdf" name="File"> 
-	<p>Expiry Date :</p>
-	<input type "date" name ="ExpDate">
-    <input TYPE="submit" name="upload" value="Submit"/>
-</form>
-
+<html> 
+ 
 
  
 
-<?php
-	if(isset($_POST['upload'])){
-		$Date = time();
-		$_POST['ExpDate'];
-		$_FILES['File']['name'];
+<body> 
+ 
+ 
+ 
+<form method="post" action="AddDaily.php" enctype="multipart/form-data"> 
+    <p>File :</p> 
+    <input type="file" accept ="application/pdf" name="File">  
+ 	<p>Expiry Date :</p>
+ 	<input type "date" name ="ExpDate">
+	<p>File Type :</p>
+	<input type="radio" name="Type" value="1" checked> Daily Notice<br>
+	<input type="radio" name="Type" value="2"> News Letter<br>
+    <input TYPE="submit" name="upload" value="Submit"/> 
+</form> 
+ 
+ 
+ 
+
+   
+ 
+
+<?php 
+ 	if(isset($_POST['upload'])){ 
+	
+		print_r($_FILES["File"]);
+ 		$Date = time(); 
+ 		$_POST['ExpDate']; 
+ 		$_FILES['File']['name']; 
 
 		
-		$servername = 'localhost';
-		$username = 'DevAdmin';
-		$password = 'test';
-		$db_name = 'sixthapp';
-		$con = mysqli_connect($servername, "root", "", $db_name);
-		$sql="insert into `files` (`Name`, `AddedDate`, `ExpiryDate`, `Link`) values('".$_FILES['File']['name']."', '".$Date."', '".$_POST['ExpDate']."','".$_FILES['File']["name"]."')";
+ 		echo $_POST['Type'];
+		
+		$servername = 'localhost'; 
+		$username = 'DevAdmin'; 
+ 		$password = 'test'; 
+ 		$db_name = 'sixthapp'; 
+ 		$con = mysqli_connect($servername, "root", "", $db_name); 
+ 		$sql="insert into `files` (`Name`, `AddedDate`, `ExpiryDate`, `Type`, `Link`) values('".$_FILES['File']['name']."', '".$Date."', '".$_POST['ExpDate']."', '".$_POST['Type']."','".$_FILES['File']["tmp_name"]."')"; 
+ 		
+		
+		#header('Content-type: application/pdf');
+		#header('Content-Disposition: inline; filename="' . $_FILES['File']['name'] . '"');
+		#header('Content-Transfer-Encoding: binary');
+		#header('Accept-Ranges: bytes');
+		#@readfile($_FILES['File']["tmp_name"]);
+ 	 
+	 
+ 		if(mysqli_query($con, $sql)){ 
+ 			echo 'data added'; 
+ 		} 
+ 		else{ 
+ 			echo'failed'; 
+			
+
+ 		} 
+ 	}
 	
 	
-		if(mysqli_query($con, $sql)){
-			echo 'data added';
-		}
-		else{
-			echo'failed';
-		}
-	}
-?>
+	
+	
+
+	
+?>	
+	<iframe src=\"$_FILES['File']['name']\" width=\"100%\" style=\"height:100%\"></iframe>
+
+ 
+ 
+ 
+ 
+ 
+ 
+</body> 
+ 
+ 
+ 
+ 
+</html> 
 
 
 
-</body>
-
-
-</html>
