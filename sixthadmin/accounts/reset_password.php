26b4972555eca2aea3f1c513ead2b098c184b392
fboyle2001 <finlayboyle2001@gmail.com>
2018-07-05 09:39:48 +0100

    Reset Password from Admin Page

---
+++ sixthadmin/accounts/reset_password.php
@@ -0,0 +1,35 @@
+<?php
+  require($_SERVER["DOCUMENT_ROOT"] . "/sixthadmin/resources/php/shared.php");
+  require($_SERVER["DOCUMENT_ROOT"] . "/sixthadmin/resources/php/Reply.php");
+
+  rejectGuest();
+
+  $reply = new Reply();
+  $id = post("id");
+
+  if($id == null) {
+    $reply->setStatus(ReplyStatus::withData(400, "No ID set"));
+    die($reply->toJson());
+  }
+
+  $selectQuery = "SELECT * FROM `accounts` WHERE `ID` = '$id'";
+  $selectQuery = DatabaseHandler::getInstance()->executeQuery($selectQuery);
+
+  if($selectQuery->wasDataReturned() == false) {
+    $reply->setStatus(ReplyStatus::withData(400, "Invalid ID"));
+    die($reply->toJson());
+  }
+
+  $defaultPassword = password_hash("Passw0rd", PASSWORD_BCRYPT);
+
+  $resetQuery = "UPDATE `accounts` SET `Password` = '$defaultPassword' WHERE `ID` = '$id'";
+  $resetQuery = DatabaseHandler::getInstance()->executeQuery($resetQuery);
+
+  if($resetQuery->wasSuccessful() == false) {
+    $reply->setStatus(ReplyStatus::withData(500, "Unable to reset password"));
+    die($reply->toJson());
+  }
+
+  $reply->setStatus(ReplyStatus::withData(200, "Successfully reset password"));
+  echo $reply->toJson();
+?>
--- sixthadmin/resources/javascript/accounts.js
+++ sixthadmin/resources/javascript/accounts.js
@@ -27,8 +27,34 @@ function process(result) {
 	} else {
 		$("#account_table").append("<tbody>");
 		$.each(result["content"]["records"], function (index, item) {
-			$("#account_table > tbody").append('<tr><td>' + item["ID"] + '</td><td>' + item["Username"] + '</td><td>' + (item["IsAdmin"] == 0 ? "No" : "Yes") + '</td><td>Fill</td><td>Fill</td></tr>');
+			$("#account_table > tbody").append('<tr><td>' + item["ID"] + '</td><td>' + item["Username"] + '</td><td>' + (item["IsAdmin"] == 0 ? "No" : "Yes") + '</td><td id="reset_' + item["ID"] + '"><a id="reset_link_' + item["ID"] + '" href="javascript:reset(' + item["ID"] + ')">Reset</a></td><td>Fill</td></tr>');
 		});
 		$("#account_table").append("</tbody>");
 	}
+}
+
+function reset(id) {
+	//disable the button
+	$("#reset_link_" + id).remove();
+	$("#reset_" + id).text("Resetting...");
+	console.log("removed");
+	var queryUrl = "http://localhost/sixthadmin/accounts/reset_password.php";
+	$.ajax({
+		url: queryUrl,
+		type: "post",
+		dataType: "json",
+		data: "id=" + id,
+		success: function(data) {
+			processResetResult(data, id);
+		},
+		error: function(data) {
+			alert("An unexpected error occurred.");
+		}
+	});
+}
+
+function processResetResult(data, id) {
+	var status = data["status"];
+	var code = status["code"];
+	$("#reset_" + id).text(status["description"]);
 }
