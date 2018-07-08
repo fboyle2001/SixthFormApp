59cfb738689f5eba2ccd5c5cc1fed7e6c120fcd8
fboyle2001 <finlayboyle2001@gmail.com>
2018-07-08 13:37:49 +0100

    Greatly improved security of application on resuming the application

--- client/www/javascript/query.js
+++ client/www/javascript/query.js
@@ -1,15 +1,3 @@
-function isLoggedIn() {
-  if(Cookies.get("auth") === undefined) {
-    return false;
-  }
-
-  if(Cookies.get("base") === undefined) {
-    return false;
-  }
-
-  return true;
-}
-
 function query(url, postData, callback, fatal) {
   if(!isLoggedIn()) {
     var response = {
@@ -44,3 +32,64 @@ function query(url, postData, callback, fatal) {
 function openInBrowser(url) {
 	cordova.InAppBrowser.open(url, "_system", "location=yes");
 }
+
+function isLoggedIn() {
+  if(Cookies.get("auth") === undefined) {
+    return false;
+  }
+
+  if(Cookies.get("base") === undefined) {
+    return false;
+  }
+
+  if(Cookies.get("resource_base") === undefined) {
+    return false;
+  }
+
+  return true;
+}
+
+function verifyUser(start, fatal) {
+  if(Cookies.get("auth") === undefined) {
+    return fatal("You are not logged in.");
+  }
+
+  if(Cookies.get("base") === undefined) {
+    return fatal("You are not logged in.");
+  }
+
+  if(Cookies.get("resource_base") === undefined) {
+    return fatal("You are not logged in.");
+  }
+
+  query("/accounts/details/", {}, function (data) {
+    var code = data["status"]["code"];
+
+    if(code == 200) {
+      start();
+    } else {
+      fatal("Session has expired.");
+    }
+  }, function (data) {
+    fatal("Unable to locate server.");
+  });
+}
+
+function onDeviceReady() {
+  console.log("ready");
+  $(document).ready(function () {
+    loadPage();
+  });
+}
+
+function onResume() {
+  console.log("resuming");
+  verifyUser(function () {
+    return;
+  }, function (err) {
+    window.location = "index.html";
+  });
+}
+
+document.addEventListener("resume", onResume, false);
+document.addEventListener("deviceready", onDeviceReady, false);
