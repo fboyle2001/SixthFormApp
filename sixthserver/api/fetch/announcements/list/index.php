55f30a6210f2cc3b4e97e1469e00d26a0b7d73cd
fboyle2001 <finlayboyle2001@gmail.com>
2018-07-12 22:39:34 +0100

    Announcements are now only visible to their set groups in the app.

--- sixthserver/api/fetch/announcements/list/index.php
+++ sixthserver/api/fetch/announcements/list/index.php
@@ -17,14 +17,68 @@
 		die($reply->toJson());
 	}
 
+  $username = get_username();
 
-	$selectLatest = "SELECT * FROM `announcements` ";
+  $selectId = "SELECT `ID`, `IsAdmin`, `Year` FROM `accounts` WHERE `Username` = '$username'";
+  $selectId = DatabaseHandler::getInstance()->executeQuery($selectId);
 
-  if($contains != null) {
-    $selectLatest .= "WHERE `Title` LIKE '%$contains%' OR `Content` LIKE '%$contains%'";
+  if($selectId->wasDataReturned() == false) {
+  	$reply->setStatus(ReplyStatus::withData(500, "Unable to get ID"));
+  	die($reply->toJson());
   }
 
-  $selectLatest .= "ORDER BY `ID` DESC LIMIT $limit";
+  $record = $selectId->getRecords()[0];
+  $id = $record["ID"];
+  $admin = $record["IsAdmin"];
+  $year = $record["Year"];
+
+  $selectGroups = "SELECT `GroupID` from `grouplink` WHERE `AccountID` = $id";
+  $selectGroups = DatabaseHandler::getInstance()->executeQuery($selectGroups);
+
+  $groupList = [-999];
+
+  if($admin == 1) {
+    array_push($groupList, -996);
+  } else {
+    if($year == 12) {
+      array_push($groupList, -998);
+    } else {
+      array_push($groupList, -997);
+    }
+  }
+
+  if($selectGroups->wasDataReturned() == true) {
+    foreach($selectGroups->getRecords() as $record) {
+      array_push($groupList, $record["GroupID"]);
+    }
+  }
+
+  $sqlList = "";
+
+  foreach($groupList as $group) {
+    if($sqlList != "") {
+      $sqlList .= ", ";
+    }
+
+    $sqlList .= $group;
+  }
+
+  $sqlList = "($sqlList)";
+
+	$selectLatest = "SELECT * FROM `announcements` ";
+  $where = "";
+
+  if($contains != null) {
+    $where = "WHERE `Title` LIKE '%$contains%' OR `Content` LIKE '%$contains%' ";
+  }
+
+  if($where != "") {
+    $where .= "AND `GroupID` IN $sqlList";
+  } else {
+    $where = "WHERE `GroupID` IN $sqlList";
+  }
+
+  $selectLatest .= "$where ORDER BY `ID` DESC LIMIT $limit";
 	$selectLatest = DatabaseHandler::getInstance()->executeQuery($selectLatest);
 
 	$reply->setStatus(ReplyStatus::withData(200, "Success"));
