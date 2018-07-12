d3dcc9518f3e0fd8b3a4b01f7fae6044d5f3db7d
fboyle2001 <finlayboyle2001@gmail.com>
2018-07-12 18:50:57 +0100

    Groups can be deleted whilst maintaining referential integrity

---
+++ sixthadmin/announcements/groups/delete.php
@@ -0,0 +1,41 @@
+<?php
+  require($_SERVER["DOCUMENT_ROOT"] . "/sixthadmin/resources/php/shared.php");
+  require($_SERVER["DOCUMENT_ROOT"] . "/sixthadmin/resources/php/Reply.php");
+
+  rejectGuest();
+
+  $reply = new Reply();
+  $id = post("id");
+
+  if($id == null) {
+    $reply->setStatus(ReplyStatus::withData(400, "No ID set"));
+    die($reply->toJson());
+  }
+
+  $selectQuery = "SELECT * FROM `groups` WHERE `ID` = '$id'";
+  $selectQuery = DatabaseHandler::getInstance()->executeQuery($selectQuery);
+
+  if($selectQuery->wasDataReturned() == false) {
+    $reply->setStatus(ReplyStatus::withData(400, "Invalid ID"));
+    die($reply->toJson());
+  }
+
+  $deleteQuery = "DELETE FROM `groups` WHERE `ID` = '$id'";
+  $deleteQuery = DatabaseHandler::getInstance()->executeQuery($deleteQuery);
+
+  if($deleteQuery->wasSuccessful() == false) {
+    $reply->setStatus(ReplyStatus::withData(500, "Unable to delete group"));
+    die($reply->toJson());
+  }
+
+  $deleteQuery = "DELETE FROM `grouplink` WHERE `GroupID` = '$id'";
+  $deleteQuery = DatabaseHandler::getInstance()->executeQuery($deleteQuery);
+
+  if($deleteQuery->wasSuccessful() == false) {
+    $reply->setStatus(ReplyStatus::withData(500, "Deleted group but unable to remove members"));
+    die($reply->toJson());
+  }
+
+  $reply->setStatus(ReplyStatus::withData(200, "Successfully deleted group"));
+  echo $reply->toJson();
+?>
