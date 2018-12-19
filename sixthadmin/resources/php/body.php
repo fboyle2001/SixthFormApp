946a967ef9cc27e13664a11ef42498b7662b9808
fboyle2001 <finlayboyle2001@gmail.com>
2018-12-19 21:00:52 +0000

    Fixed navigation bar for mobile

--- sixthadmin/resources/php/body.php
+++ sixthadmin/resources/php/body.php
@@ -1,10 +1,10 @@
 <div class="navigation">
   <nav>
     <ul>
-      <a href="/sixthadmin/accounts/"><li data-content-key="accounts">Accounts</li></a>
-      <a href="/sixthadmin/announcements/"><li data-content-key="announcements">Announcements</li></a>
-  		<a href="/sixthadmin/files/"><li data-content-key="files">Files</li></a>
-  		<a href="/sixthadmin/links/"><li data-content-key="links">Links</li></a>
+      <li data-content-key="accounts">Accounts</li>
+      <li data-content-key="announcements">Announcements</li>
+  		<li data-content-key="files">Files</li>
+  		<li data-content-key="links">Links</li>
   		<a href="/sixthadmin/accounts/logout.php"><li data-content-key="logout">Logout</li></a>
 		</ul>
   </nav>
