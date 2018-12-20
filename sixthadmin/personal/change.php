a899ba1daf3353fb5265b84f99b21e64fe134b43
fboyle2001 <finlayboyle2001@gmail.com>
2018-12-20 21:52:47 +0000

    Allow admins to change password from admin site

---
+++ sixthadmin/personal/change.php
@@ -0,0 +1,67 @@
+<?php
+	// Includes the shared PHP resource page which includes useful functions for data conversion
+	// And it includes the necessary code for connecting to the database
+	require("../shared.php");
+
+	// Calls a function from the shared page which prevents non-logged in users from accessing the page
+	// Guests will be redirect to the login page
+	rejectGuest();
+
+	$message = "";
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		if(!has_arg("POST", "password")) {
+			$message = "No password set";
+		} else {
+			if(!has_arg("POST", "confirmation")) {
+				$message = "No confirmation of password set";
+			} else {
+				$password = post("password");
+				$confirmation = post("confirmation");
+
+				if($password !== $confirmation) {
+					$message = "Passwords do not match";
+				} else {
+					$hashedPassword = password_hash($password, PASSWORD_BCRYPT, ["cost" => $cost]);
+
+					$update = Database::get()->prepare("UPDATE `accounts` SET `Password` = :password WHERE `Username` = :username");
+					$success = $update->execute(["password" => $hashedPassword, "username" => $_SESSION["user"]]);
+
+					if($success === true) {
+						$message = "Updated password";
+					} else {
+						$message = "Failed to update password";
+					}
+				}
+			}
+		}
+	}
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+			// Includes the default header which includes the stylesheet and navigation JavaScript
+			// It also includes jQuery in the page
+			require($_SERVER["DOCUMENT_ROOT"] . "/sixthadmin/resources/php/head.php");
+		?>
+    <script src="/sixthadmin/resources/javascript/change-password.js"></script>
+  </head>
+  <body>
+    <?php
+			// Includes the default body which includes the navigation menu at the top of the page
+			require($_SERVER["DOCUMENT_ROOT"] . "/sixthadmin/resources/php/body.php");
+		?>
+    <div>
+      <h1>Change Password</h1>
+      <form method="POST">
+        <table>
+          <tr><td>New Password</td><td><input type="password" name="password" id="password"></td><td><span id="password_standard"></span></td></tr>
+          <tr><td>Confirm Password</td><td><input type="password" name="confirmation" id="confirmation"></td><td><span id="confirm_status"></span></tr>
+          <tr id="submission" style="display: none;"><td colspan="3"><input type="submit" value="Change"></td></tr>
+        </table>
+      </form>
+			<p><?php echo $message; ?></p>
+    </div>
+  </body>
+</html>
--- sixthadmin/resources/php/body.php
+++ sixthadmin/resources/php/body.php
@@ -5,7 +5,7 @@
       <li data-content-key="announcements">Announcements</li>
   		<li data-content-key="files">Files</li>
   		<li data-content-key="links">Links</li>
-  		<a href="/sixthadmin/accounts/logout.php"><li data-content-key="logout">Logout</li></a>
+      <li data-content-key="personal">My Account</li>
 		</ul>
   </nav>
 </div>
