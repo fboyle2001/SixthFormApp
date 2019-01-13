0489950927bf2c780e33e426c35027016fdd5f6a
fboyle2001 <finlayboyle2001@gmail.com>
2019-01-13 14:01:58 +0000

    Added better error messages when session expires

--- client/www/javascript/settings_loader.js
+++ client/www/javascript/settings_loader.js
@@ -141,7 +141,7 @@ function produceStyleElement() {
 function produceViewportElement() {
   var scalableValue = getUserSettings().scalable ? "yes" : "no";
 
-  var tag = '<meta name="viewport" content="user-scalable=' + scalableValue + ', initial-scale=1, maximum-scale=2, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi", viewport-fit=cover/>';
+  var tag = '<meta name="viewport" content="user-scalable=' + scalableValue + ', initial-scale=1, maximum-scale=2, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi, viewport-fit=cover"/>';
   return tag;
 }
 
