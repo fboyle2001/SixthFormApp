1609c27b6bfcbd6e63bbad79fb117c157c209398
fboyle2001 <finlayboyle2001@gmail.com>
2019-01-17 14:27:12 +0000

    Stopped links opening in main view from credits

--- client/www/javascript/credits.js
+++ client/www/javascript/credits.js
@@ -5,3 +5,12 @@ $(document).ready(function() {
     window.location = "index.html";
   });
 });
+
+// Opens a link in the browser
+function openInBrowser(url) {
+  if(typeof cordova !== "undefined" && typeof cordova.InAppBrowser !== "undefined") {
+    cordova.InAppBrowser.open(url, "_system");
+  } else {
+    window.open(url, "_blank");
+  }
+}
